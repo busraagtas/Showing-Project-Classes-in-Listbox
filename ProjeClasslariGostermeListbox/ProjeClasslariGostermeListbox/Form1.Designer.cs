fbfbfbe82257eea6b0414ea6ba4051c41a22b9fd
Büşra AĞTAŞ <agbusra658@gmail.com>
2025-04-24 17:38:23 +0300

    Initial commit

---
+++ ProjeClasslariGostermeListbox/ProjeClasslariGostermeListbox/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ProjeClasslariGostermeListbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKlasorSec = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnKlasorSec
+            // 
+            this.btnKlasorSec.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnKlasorSec.Location = new System.Drawing.Point(158, 84);
+            this.btnKlasorSec.Name = "btnKlasorSec";
+            this.btnKlasorSec.Size = new System.Drawing.Size(224, 41);
+            this.btnKlasorSec.TabIndex = 0;
+            this.btnKlasorSec.Text = "Klasörleri Seç";
+            this.btnKlasorSec.UseVisualStyleBackColor = false;
+            this.btnKlasorSec.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(403, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Classları görmek istiyorsanız butona basıp proje klasörünü seçin ❗";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(109, 131);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(328, 244);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(560, 455);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKlasorSec);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKlasorSec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ProjeClasslariGostermeListbox/ProjeClasslariGostermeListbox/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjeClasslariGostermeListbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                string rootPath = dialog.SelectedPath;
+                string[] directories = Directory.GetDirectories(rootPath);
+
+                foreach (string dir in directories)
+                {
+                    string folderName = Path.GetFileName(dir);
+                    listBox1.Items.Add($"📁 {folderName}");
+
+                    string[] csFiles = Directory.GetFiles(dir, "*.cs", SearchOption.TopDirectoryOnly);
+                    foreach (string file in csFiles)
+                    {
+                        string[] lines = File.ReadAllLines(file);
+                        foreach (string line in lines)
+                        {
+                            if (line.Trim().Contains("class "))
+                            {
+                                string className = GetClassName(line);
+                                if (!string.IsNullOrEmpty(className))
+                                {
+                                    listBox1.Items.Add($"   📄 {className}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private string GetClassName(string line)
+        {
+            Match match = Regex.Match(line, @"class\s+(\w+)");
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            return null;
+        }
+    }
+}
